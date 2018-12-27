8b1e2faec8068d17d5112233ee74bc5b5aec601d
joamag <joamag@hive.pt>
2018-12-27 19:12:46 +0000

    Initial code structure for RIPE ID API

---
+++ src/ripe_id/base.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import appier
+
+RIPE_BASE_URL = "https://id.platforme.com/api/"
+""" The default base URL to be used when no other
+base URL value is provided to the constructor """
+
+class API(appier.OAuth2API):
+
+    def __init__(self, *args, **kwargs):
+        appier.API.__init__(self, *args, **kwargs)
+        self.base_url = appier.conf("RIPE_ID_BASE_URL", RIPE_BASE_URL)
+        self.username = appier.conf("RIPE_USERNAME", None)
+        self.password = appier.conf("RIPE_PASSWORD", None)
+        self.secret_key = appier.conf("RIPE_SECRET_KEY", None)
+        self.admin = appier.conf("RIPE_ADMIN", True, cast = bool)
+        self.base_url = kwargs.get("base_url", self.base_url)
+        self.username = kwargs.get("username", self.username)
+        self.password = kwargs.get("password", self.password)
+        self.secret_key = kwargs.get("secret_key", self.secret_key)
+        self.admin = kwargs.get("admin", self.admin)
+        self.session_id = kwargs.get("session_id", None)
+
+    def build(
+        self,
+        method,
+        url,
+        data = None,
+        data_j = None,
+        data_m = None,
+        headers = None,
+        params = None,
+        mime = None,
+        kwargs = None
+    ):
+        auth = kwargs.pop("auth", True)
+        if auth and self.secret_key: headers["X-Secret-Key"] = self.secret_key
+        if auth and not self.secret_key: params["sid"] = self.get_session_id()
+
+    def get_session_id(self):
+        if self.session_id: return self.session_id
+        return self.login()
+
+    def auth_callback(self, params, headers):
+        self.session_id = None
+        session_id = self.get_session_id()
+        params["session_id"] = session_id
+
+    def login(self, username = None, password = None, admin = None):
+        username = username or self.username
+        password = password or self.password
+        admin = admin or self.admin
+        url = self.base_url + ("signin_admin" if admin else "signin")
+        contents = self.post(
+            url,
+            callback = False,
+            auth = False,
+            username = username,
+            password = password
+        )
+        self.username = contents.get("username", None)
+        self.session_id = contents.get("session_id", None)
+        self.tokens = contents.get("tokens", None)
+        self.trigger("auth", contents)
+        return self.session_id
+
+    def is_auth(self):
+        if not self.username: return False
+        if not self.password: return False
+        return True
