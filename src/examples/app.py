11a1e93c0986f2e6e6f5f607bf7aaa068f33569b
joamag <joamag@gmail.com>
2018-12-27 22:20:26 +0000

    Initial demo code

--- src/examples/app.py
+++ src/examples/app.py
@@ -16,16 +16,44 @@ class RipeIdApp(appier.WebApp):
 
     @appier.route("/", "GET")
     def index(self):
-        return self.config()
+        return self.me()
 
-    @appier.route("/config", "GET")
-    def config(self):
+    @appier.route("/me", "GET")
+    def me(self):
+        url = self.ensure_api()
+        if url: return self.redirect(url)
         api = self.get_api()
-        config = api.get_config()
-        return config
+        user = api.self_user()
+        return user
+
+    @appier.route("/oauth", "GET")
+    def oauth(self):
+        code = self.field("code")
+        api = self.get_api()
+        access_token = api.oauth_access(code)
+        self.session["rid.access_token"] = access_token
+        return self.redirect(
+            self.url_for("google.index")
+        )
+
+    @appier.exception_handler(appier.OAuthAccessError)
+    def oauth_error(self, error):
+        if "rid.access_token" in self.session: del self.session["rid.access_token"]
+        return self.redirect(
+            self.url_for("ripe_id.index")
+        )
+
+    def ensure_api(self):
+        access_token = self.session.get("ff.access_token", None)
+        if access_token: return
+        api = base.get_api()
+        return api.oauth_authorize()
 
     def get_api(self):
-        return base.get_api()
+        access_token = self.session and self.session.get("rid.access_token", None)
+        api = base.get_api()
+        api.access_token = access_token
+        return api
 
 if __name__ == "__main__":
     app = RipeIdApp()
