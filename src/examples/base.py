446edf446c6bef6e830ae3ddd1da73acd7f4dd0d
joamag <joamag@gmail.com>
2018-12-27 22:21:11 +0000

    Small fix

--- src/examples/base.py
+++ src/examples/base.py
@@ -3,15 +3,15 @@
 
 import appier
 
-import ripe
+import ripe_id
 
 SCOPE = (
-    "email",
-    "acl"
+    "acl",
+    "email"
 )
 
 def get_api():
-    return ripe.API(
+    return ripe_id.API(
         client_id = appier.conf("RIPE_ID_ID"),
         client_secret = appier.conf("RIPE_ID_SECRET"),
         redirect_url = appier.conf("RIPE_ID_REDIRECT_URL"),
