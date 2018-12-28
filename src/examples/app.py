96aed52e77d548efa9f8dc22e569196659552501
joamag <joamag@hive.pt>
2018-12-28 11:35:08 +0000

    Support for the logout route and for the OAuth error validation

--- src/examples/app.py
+++ src/examples/app.py
@@ -26,9 +26,19 @@ class RipeIdApp(appier.WebApp):
         account = api.self_account()
         return account
 
+    @appier.route("/logout", "GET")
+    def logout(self):
+        return self.oauth_error(None)
+
     @appier.route("/oauth", "GET")
     def oauth(self):
         code = self.field("code")
+        error = self.field("error")
+        appier.verify(
+            not error,
+            message = "Invalid OAuth response (%s)" % error,
+            exception = appier.OperationalError
+        )
         api = self.get_api()
         access_token = api.oauth_access(code)
         self.session["rid.access_token"] = access_token
