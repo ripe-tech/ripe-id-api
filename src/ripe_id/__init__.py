d1bf401a776989e2495f37b622f24addcde3bde6
joamag <joamag@gmail.com>
2019-04-03 16:43:06 +0100

    New import values

--- src/ripe_id/__init__.py
+++ src/ripe_id/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from . import account
 from . import base
+from . import token
 
+from .account import AccountAPI
 from .base import API
+from .token import TokenAPI
